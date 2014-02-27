6cc2b3ca1f102f098c8e4823022ff79fe1b3330c
Tobias Becker <rh-clash@web.de>
2014-02-27 12:17:30 +0100

    Solved 6th problem.

--- 20090219_flavius-josephus.fsx
+++ 20090219_flavius-josephus.fsx
@@ -1,1 +1,15 @@
-﻿
+﻿let flavius n m =
+    let m = m - 1
+    let rec loop i alive dead =
+        let n = Array.length alive
+        if n = 0 then
+            List.rev dead
+        elif i >= n then
+            loop (i % n) alive dead
+        else
+            let shot = alive.[i]
+            loop (i + m) (Array.filter ((<>)shot) alive) (shot::dead)
+    loop m [| 0..n-1 |] []
+
+flavius 41 3
+    |> Seq.iter (printfn "%d")
