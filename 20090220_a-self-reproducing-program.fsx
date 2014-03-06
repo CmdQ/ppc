6e10cc6e77ae70a755a11371a1c69792e39f0947
Tobias Becker <rh-clash@web.de>
2014-03-06 15:15:05 +0100

    Rather using an unused symbol.

--- 20090220_a-self-reproducing-program.fsx
+++ 20090220_a-self-reproducing-program.fsx
@@ -8,7 +8,7 @@ let replace (needle:char) (haystack:string) straw =
     let index = haystack.IndexOf(needle)
     haystack.Remove(index, 1).Insert(index, straw)
 let threeQuotes = String(Array.init 3 (fun _ -> '"'))
-let prog = %
-replace '%' prog (threeQuotes + prog + threeQuotes) |> printf "%s"
+let prog = $
+replace '$' prog (threeQuotes + prog + threeQuotes) |> printf "%s"
 """
-replace '%' prog (threeQuotes + prog + threeQuotes) |> printf "%s"
+replace '$' prog (threeQuotes + prog + threeQuotes) |> printf "%s"
